06975cfedf29053ca04cd40ef32223997286fe86
sammiee5311 <sammiee5311@gmail.com>
2023-02-02 20:45:36 +0900

    Add hooks

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+
+def remove(filepath):
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+    elif os.path.isdir(filepath):
+        shutil.rmtree(filepath)
+
+
+if "{{ cookiecutter.open_source_license }}" == "Not open source":
+    remove(os.path.join(os.getcwd(), "{{cookiecutter.project_slug}}", "LICENSE"))
+
+
+print("Generated files from cookiecutter.")
