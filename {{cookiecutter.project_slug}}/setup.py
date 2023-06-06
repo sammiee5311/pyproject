45e709fc232629082b65a1259b3715fdc4ae87b8
sammiee5311 <sammiee5311@gmail.com>
2023-06-06 14:52:58 +0900

    Fix missing comma

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 dev_requires = [
     "pytest",
-    "pytest-cov"
+    "pytest-cov",
     "pytest-env",
     {% if cookiecutter.black|lower == 'y' -%}
     "black",
